e8bf7bfaea6510ad39947b37bd95003efcd88a9c
gualter_apple@hotmail.com <gualter_apple@hotmail.com>
2023-01-22 16:22:14 +0100

    Enviando uma resposta depois de receber um post

--- index.js
+++ index.js
@@ -12,5 +12,6 @@ app.route('/sobre').get((req, res) => res.send('Hello sobre'))*/
 //middleware
 app.use(express.json())
 
-
-app.route('/').post((req, res) => console.log(req.body))
+/** USANDO O POST*/
+//app.route('/').post((req, res) => console.log(req.body))
+app.route('/').post((req, res) => res.send(req.body))
