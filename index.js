07c5757dea817bd0ad4419daf08a25c5ec83fd78
gualter_apple@hotmail.com <gualter_apple@hotmail.com>
2023-01-22 16:53:28 +0100

    Delete feito

--- index.js
+++ index.js
@@ -17,11 +17,17 @@ app.use(express.json())
 //app.route('/').post((req, res) => res.send(req.body))
 
 /** USANDO O PUT*/
-let author = 'Gualter Santos'
+/*let author = 'Gualter Santos'
 
 app.route('/').get((req, res) => res.send(author))
 
 app.route('/').put((req, res) => {
     author = req.body.author
     res.send(author)
+})*/
+
+
+
+app.route('/:identificador').delete((req, res) => {
+    res.send(req.params.identificador)
 })
