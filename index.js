e6aa5bd90fd152a5d3d57df055196aa4aaa7e21e
gualter_apple@hotmail.com <gualter_apple@hotmail.com>
2023-01-22 16:50:00 +0100

    PUT feito

--- index.js
+++ index.js
@@ -14,4 +14,14 @@ app.use(express.json())
 
 /** USANDO O POST*/
 //app.route('/').post((req, res) => console.log(req.body))
-app.route('/').post((req, res) => res.send(req.body))
+//app.route('/').post((req, res) => res.send(req.body))
+
+/** USANDO O PUT*/
+let author = 'Gualter Santos'
+
+app.route('/').get((req, res) => res.send(author))
+
+app.route('/').put((req, res) => {
+    author = req.body.author
+    res.send(author)
+})
